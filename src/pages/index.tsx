3af3a622392fc3c76ae4d642f87f256a78335f3f
Abhi Aiyer <abhiaiyer91@gmail.com>
2023-12-26 16:13:13 -0800

    Fix build

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -77,7 +77,6 @@ import { Input } from "@/components/ui/input";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 import { useState } from "react";
-import dynamic from "next/dynamic";
 import { supabase } from "@/lib/supabase";
 import { useRouter } from "next/router";
 import { UserNav } from "@/components/layout";
@@ -1233,19 +1232,47 @@ function AppView({
   }
 
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>The Basics</CardTitle>
-      </CardHeader>
+    <div className="grid gap-4 justify-center lg:grid-cols-2">
+      <Card>
+        <CardHeader>
+          <CardTitle>The Basics</CardTitle>
+        </CardHeader>
 
-      <CardContent>
-        <Fitness
-          userConfig={userConfig}
-          userId={userConfig?.userId}
-          refetch={refetch}
-        />
-      </CardContent>
-    </Card>
+        <CardContent>
+          <Fitness
+            userConfig={userConfig}
+            userId={userConfig?.userId}
+            refetch={refetch}
+          />
+        </CardContent>
+      </Card>
+      <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
+        <div className="absolute inset-0 bg-zinc-900" />
+        <div className="relative z-20 flex items-center text-lg font-medium">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="mr-2 h-6 w-6"
+          >
+            <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
+          </svg>
+          Journey
+        </div>
+        <div className="relative z-20 items-center text-lg">
+          <p>Everyone starts their journey somewhere.</p>
+          <p>
+            Fill out the information to the left. 
+          </p>
+          <p>This will get your baseline in
+            order.</p>
+        </div>
+      </div>
+    </div>
   );
 }
 
